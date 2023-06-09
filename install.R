8dd78d45a039f710932c42770a45f813c6fad572
Julien Barde <juldebar@users.noreply.github.com>
2023-06-09 16:25:27 +0200

    Create install.R

---
+++ install.R
@@ -0,0 +1,17 @@
+package <- jsonlite::read_json('./srv/dockerization_shiny/package.json')
+invisible(lapply(package$dependencies, function(pkg){
+  from <- 'cran'
+  pkg_installer <- remotes::install_version
+  if(!is.null(pkg$from)){
+    from <- pkg$from
+    pkg_installer <- try(eval(parse(text=paste0("remotes::install_",from))))
+  }
+  if(class(pkg_installer)[1] == "try-error") return(NULL)
+  version <- ""
+  if(!is.null(pkg$version)) version <- paste0("[",pkg$version,"]")
+  cat(sprintf("Install package '%s' %s from '%s'\n", pkg$package, version, from))
+  pkg_args <- pkg[!sapply(names(pkg), function(x){x %in% c("from","dependencies")})]
+  pkg_deps <- NA
+  if(!is.null(pkg$dependencies)) pkg_deps <- pkg$dependencies
+  do.call(pkg_installer, c(pkg_args, dependencies = pkg_deps))
+}))
